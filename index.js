df81f279d5cbcce5cadd4504a83bde44e12f1f3f
Maxence LEFEBVRE <maxence.lefebvre@soprasteria.com>
2018-06-06 16:06:15 +0200

    feat(helmet): add helmet middleware

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const app = require('express')();
-const proxy = require('http-proxy-middleware');
 const HttpsProxyAgent = require('https-proxy-agent');
-const { middleware: cache } = require('apicache');
-const cors = require('cors');
 
 const env = process.env;
 const options = {target: env.PROXY_TARGET, changeOrigin: true};
@@ -12,8 +9,10 @@ if (proxyServer) {
     options.agent = new HttpsProxyAgent(proxyServer);
 }
 
-app.use(cors('*'));
-app.use(cache(env.CACHE_DURATION || '1 day'));
-app.use('/api', proxy(options));
+app.use(require('helmet')());
+app.use(require('cors')('*'));
+app.use(require('apicache').middleware(env.CACHE_DURATION || '1 day'));
+
+app.use('/api', require('http-proxy-middleware')(options));
 
 app.listen(env.APP_PORT || env.PORT || 3000);
